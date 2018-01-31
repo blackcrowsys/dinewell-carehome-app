5b2ea5260bd3148f10e5d07e9510daf54e4668de
rahulsinghbcs <rahul.singh@blackcrowsys.com>
2018-01-31 14:08:28 +0000

    [#154036434] Handle unauthorized scenario. Bump up Kotlin version and add Robolectric for unit tests - it's needed now.

--- app/src/main/java/com/blackcrowsys/exceptions/ErrorMapper.kt
+++ app/src/main/java/com/blackcrowsys/exceptions/ErrorMapper.kt
@@ -2,13 +2,23 @@ package com.blackcrowsys.exceptions
 
 import android.content.Context
 import com.blackcrowsys.R
+import com.jakewharton.retrofit2.adapter.rxjava2.HttpException
+import java.net.HttpURLConnection.HTTP_UNAUTHORIZED
 
-class ErrorMapper(val context: Context) {
+class ErrorMapper(private val context: Context) {
 
     fun transformException(throwable: Throwable): AppException {
         return when (throwable) {
             is InvalidUrlException -> AppException(context.getString(R.string.url_invalid_error))
             is EmptyUsernamePasswordException -> AppException(context.getString(R.string.empty_username_password_error))
+            is HttpException -> transformRetrofitException(throwable)
+            else -> AppException(context.getString(R.string.unknown_error))
+        }
+    }
+
+    private fun transformRetrofitException(httpException: HttpException): AppException {
+        return when (httpException.code()) {
+            HTTP_UNAUTHORIZED -> AppException(context.getString(R.string.incorrect_login_details))
             else -> AppException(context.getString(R.string.unknown_error))
         }
     }
--- app/src/main/java/com/blackcrowsys/util/SharedPreferencesHandler.kt
+++ app/src/main/java/com/blackcrowsys/util/SharedPreferencesHandler.kt
@@ -2,11 +2,9 @@ package com.blackcrowsys.util
 
 import com.f2prateek.rx.preferences2.RxSharedPreferences
 
-class SharedPreferencesHandler(private val rxSharedPreferences: RxSharedPreferences) {
+const val ENDPOINT_URL_KEY = "endpoint_url_key"
 
-    companion object {
-        const val ENDPOINT_URL_KEY = "endpoint_url_key"
-    }
+class SharedPreferencesHandler(private val rxSharedPreferences: RxSharedPreferences) {
 
     fun setEndpointUrl(url: String) = rxSharedPreferences.getString(ENDPOINT_URL_KEY).set(url)
 
